af96f237e186f073ba7f6b27054c563f0d55ed66
aniiaham <aniya_axm@icloud.com>
2024-07-07 20:45:21 -0500

    newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), solidJs()]
+  output: "server",
+  site: "https://astro-frontend-masters.netlify.app",
+  integrations: [react(), solidJs()],
 });
